6d97584150bc5a50fbf97a139e3c8f077711c314
emily <emily@bacheconsulting.com>
2022-03-15 15:16:54 +0100

    attempt at gtest parameterized test

--- cpp/test-gtest/sample_gtest2.cpp
+++ cpp/test-gtest/sample_gtest2.cpp
@@ -1,22 +1,49 @@
 #include <gtest/gtest.h>
+#include <tuple>
 
 #include "item.h"
 #include "shopping_basket.h"
 
 using namespace std;
 
-TEST(ShoppingBasket, Prices) {
-    std::vector<ItemQuantity> items = {};
-    std::vector<ItemQuantity> all_items = {ItemQuantity(1, 50),
-                                           ItemQuantity(2, 25),
-                                           ItemQuantity(1, 25),
-                                           ItemQuantity(1, 10),
-                                           ItemQuantity(1, 100)
-    };
-    std::vector<float> prices = {50, 100, 118.75, 128.25, 211.5};
-    for (unsigned int i = 0; i < 5; i++) {
-        items.push_back(all_items.at(i));
-        ASSERT_NEAR(prices.at(i), price(items), 0.01);
-    }
+class BasketCase {
+private:
+
+public:
+    BasketCase(vector<ItemQuantity> items, float expectedPrice) : _items(items), _expectedPrice(expectedPrice) {}
+
+    vector<ItemQuantity> _items;
+    float _expectedPrice;
+};
+
+class ShoppingBasketTest : public testing::TestWithParam<const BasketCase *> {
+public:
+};
+
+// Not sure why this is not used to print each basket case
+std::ostream& operator<<(std::ostream &strm, const BasketCase &basketCase) {
+    return strm << " BasketCase " << " expected price " << basketCase._expectedPrice;
 }
 
+INSTANTIATE_TEST_CASE_P
+(
+        ShoppingBasketTests,
+        ShoppingBasketTest,
+        ::testing::Values(
+
+                new BasketCase({ItemQuantity(1, 50)}, 50),
+                new BasketCase({ItemQuantity(1, 50), ItemQuantity(2, 25)}, 100),
+                new BasketCase({ItemQuantity(1, 50), ItemQuantity(2, 25), ItemQuantity(1, 25)}, 118.75),
+                new BasketCase({ItemQuantity(1, 50), ItemQuantity(2, 25), ItemQuantity(1, 25), ItemQuantity(1, 10)},
+                               128.25),
+                new BasketCase({ItemQuantity(1, 50), ItemQuantity(2, 25), ItemQuantity(1, 25), ItemQuantity(1, 10),
+                                ItemQuantity(1, 100)}, 211.5)
+
+        ));
+
+
+
+TEST_P(ShoppingBasketTest, Price) {
+    const BasketCase *basketCase = GetParam();
+    ASSERT_NEAR(basketCase->_expectedPrice, price(basketCase->_items), 0.01);
+}
